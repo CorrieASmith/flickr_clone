e46ecb15f4b6c33cc632070d65a8b48e7b86681b
Jake Glassman & Corrie Smith <student@epicodus.com>
2015-10-21 16:27:17 -0700

    add seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :pictures
+  has_many :pictures, dependent: :destroy
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "app/assets/images/default-avatar.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class User < ActiveRecord::Base
+
+  users = User.create([{ username: "Cartman", email: "cartman@southpark.com", password: "tacotaco", avatar: "http://offclouds.com/data_images/wallpapers/3/263406-cartman.jpg", id: 100 }])
+  users = User.create([{ username: "Stan", email: "stan@southpark.com", password: "tacotaco", avatar: "https://upload.wikimedia.org/wikipedia/en/a/a7/StanMarsh.png", id: 200 }])
+  users = User.create([{ username: "Kyle", email: "kyle@southpark.com", password: "tacotaco", avatar: "https://upload.wikimedia.org/wikipedia/en/2/25/KyleBroflovski.png", id: 300 }])
+  users = User.create([{ username: "Kenny", email: "kenny@southpark.com", password: "tacotaco", avatar: "https://upload.wikimedia.org/wikipedia/en/6/6f/KennyMcCormick.png", id: 400 }])
+  users = User.create([{ username: "Butters", email: "butters@southpark.com", password: "tacotaco", avatar: "http://vignette3.wikia.nocookie.net/southpark/images/c/c2/Butters_(Facebook).jpg/revision/latest?cb=20101010032409", id: 500 }])
+
+end
+
+class Picture < ActiveRecord::Base
+
+  pictures = Picture.create([{ image: "http://rs2img.memecdn.com/thug-life_o_752874.jpg", user_id: 100 }])
+  pictures = Picture.create([{ image: "http://apollo-na-uploads.s3.amazonaws.com/1438562405/my-code-doesnt-work-i-have-no-idea-why.jpg", user_id: 200 }])
+  pictures = Picture.create([{ image: "https://40.media.tumblr.com/8d93cee7236baaa36667069f9c893760/tumblr_n8hgtmhpjh1s4oyt0o1_500.jpg", user_id: 300 }])
+  pictures = Picture.create([{ image: "http://i.imgur.com/DS3vxna.jpg", user_id: 400 }])
+  pictures = Picture.create([{ image: "http://static1.fjcdn.com/comments/4753947+_080b91e89af5fa241c34100a9fa24c47.jpg", user_id: 500 }])
+
+end
